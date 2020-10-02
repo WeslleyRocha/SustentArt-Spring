9253f30e9bd51e89472cddd3cf31cd33a7557c07
Weslley Rocha <weslley.nx@gmail.com>
2020-10-02 14:34:23 -0300

    Add Arquivos Spring

---
+++ recicla/src/main/java/com/reciclarte/recicla/ReciclaApplication.java
@@ -0,0 +1,13 @@
+package com.reciclarte.recicla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciclaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciclaApplication.class, args);
+	}
+
+}
---
+++ recicla/src/test/java/com/reciclarte/recicla/ReciclaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reciclarte.recicla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciclaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
